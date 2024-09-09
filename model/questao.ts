d6e6b0a157a54a421bdda106dfdfb147f1af1a1e
Edson Garcia <ednosmab@yahoo.com.br>
2024-09-09 11:39:38 -0300

    implementada a função respondida

--- model/questao.ts
+++ model/questao.ts
@@ -1,17 +1,17 @@
+import RespostaModel from "./resposta"
+
 export default class QuestaoModel {
     #id: number
     #enunciado: string
-    #respostas: any[]
+    #respostas: RespostaModel[]
     #acertou: boolean
-    // #respondida: boolean
 
-    constructor(id: number, enunciado: string, respostas: any[], acertou = false) {
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
         this.#id = id
         this.#enunciado = enunciado
         this.#respostas = respostas
         this.#acertou = acertou
     }
-
 
     get id() {
         return this.#id
@@ -30,7 +30,10 @@ export default class QuestaoModel {
     }
 
     get respondida() {
-        // FIXME: implementar esse código
+        for (let resposta of this.#respostas) {
+            if (resposta.revelada) return true
+        }
+
         return false
     }
 }
