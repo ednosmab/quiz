d50be16b1f96c7628db9b44399bd0b07cf4cedd0
Edson Garcia <ednosmab@yahoo.com.br>
2024-09-11 17:04:00 -0300

    implementado componente resultado

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,9 +1,24 @@
-import Botao from "@/components/Botao";
+import Estatisticas from "@/components/Estatisticas"
+import styles from "../styles/Resultado.module.css"
+import { useRouter } from "next/router"
+import Botao from "@/components/Botao"
 
 export default function Resultado(){
+    const router = useRouter()
+    
+    const total = Number(router.query.total)
+    const certas = Number(router.query.certas)
+    const percentual = Math.round((certas / total) * 100)
+
     return (
-        <div>
-            <h1>Resultado</h1>
+        <div className={styles.resultado}>
+            <h1>Resultado Final</h1>
+            <div style={{display: "flex"}}>
+                <Estatisticas texto="Perguntas" valor={total}/>
+                <Estatisticas texto="Certas" valor={certas} corFundo="#9CD2A4"/>
+                <Estatisticas texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33"/>
+            </div>
+            <Botao href="/" texto="Tentar Novamente"/>
         </div>
     )
 }
