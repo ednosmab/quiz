537132f81cfa04a9532a2d5671677a3f5ec30698
Edson Garcia <ednosmab@yahoo.com.br>
2024-09-12 13:10:05 -0300

    trocado a url local da api para a url da vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "@/model/questao";
 import { useRouter } from "next/router";
 import { use, useEffect, useState } from "react";
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://quiz-ten-omega.vercel.app/api"
 
 export default function Home() {
   const router = useRouter()
